7418d4eb4e5c4757a51938bbf934efc10fb6e175
Vignesh2k0508 <bvignesh220@gmail.com>
2025-05-29 16:56:36 +0530

    Reviews completed

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -81,10 +81,17 @@ const deleteReview = async ( req, res) =>{
 
 }
 
+const getSingleProductReviews = async (req, res)=>{
+    const {id: productId} = req.params;
+    const reviews = await Review.find({product: productId})
+    res.status(StatusCodes.OK).json({reviews, count: reviews.length})
+}
+
 module.exports = {
     createReview,
     getAllReviews,
     getSingleReview,
     updateReview,
     deleteReview,
+    getSingleProductReviews,
 };
